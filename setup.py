d77d20549b8ee6bc3fcc04cd1cc83f7810f97992
Gian Luca Dalla Torre <gianluca.dallatorre@gmail.com>
2016-11-26 12:35:36 +0100

    Conditional load of enumeration on setup tools.

--- setup.py
+++ setup.py
@@ -1,31 +1,47 @@
 import codecs
-from setuptools import setup, find_packages
+import setuptools
+import sys
 
 long_description = codecs.open('README.rst', "r").read()
 
-setup(
-    name = "django-googledrive-storage",
-    version = "1.2.0",
-    author = "Gian Luca Dalla Torre",
-    author_email = "gianluca.dallatorre@gmail.com",
-    description = ("Storage implementation for Django that interacts with Google Drive"),
-    license = "LICENSE.txt",
-    keywords = "django google drive storage googledrive",
-    url = "https://github.com/torre76/django-googledrive-storage",
-    download_url = "https://github.com/torre76/django-googledrive-storage/tarball/1.2.0",
-    packages=find_packages(exclude=["django_googledrive_storage", "gdstorage.tests", "docs"]),
+# Conditional load of enumeration
+# See https://hynek.me/articles/conditional-python-dependencies/
+
+INSTALL_REQUIRES = [
+    "Django >= 1.7",
+    "google-api-python-client >= 1.5.1",
+    "python-dateutil >= 2.5.3",
+    "requests >= 2.10.0",
+    "django-appconf >= 1.0.2",
+    "oauth2client >= 2.2.0",
+    ""
+]
+
+EXTRAS_REQUIRE = dict()
+
+if int(setuptools.__version__.split(".", 1)[0]) < 18:
+    if sys.version_info[0:2] < (3, 4):
+        INSTALL_REQUIRES.append("enum34 >= 1.1.6")
+else:
+    EXTRAS_REQUIRE[":python_version<'3.4'"] = ["enum34 >= 1.1.6"]
+
+setuptools.setup(
+    name="django-googledrive-storage",
+    version="1.2.0",
+    author="Gian Luca Dalla Torre",
+    author_email="gianluca.dallatorre@gmail.com",
+    description=("Storage implementation for Django that interacts with Google Drive"),
+    license="LICENSE.txt",
+    keywords="django google drive storage googledrive",
+    url="https://github.com/torre76/django-googledrive-storage",
+    download_url="https://github.com/torre76/django-googledrive-storage/tarball/1.2.0",
+    packages=setuptools.find_packages(exclude=["django_googledrive_storage", "gdstorage.tests", "docs"]),
     long_description=long_description,
-    package_data = {
+    package_data={
         '': ['README.rst'],
     },
-    install_requires=[
-        "Django >= 1.7",
-        "google-api-python-client >= 1.5.1",
-        "python-dateutil >= 2.5.3",
-        "requests >= 2.10.0",
-        "django-appconf >= 1.0.2",
-        "oauth2client >= 2.2.0"
-    ],      
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",

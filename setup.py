31b1e5ccf6c178d79233538a137919e8ca97bbbc
Gian Luca Dalla Torre <gianluca.dallatorre@gmail.com>
2017-01-21 22:30:27 +0100

    Prepare for 1.3.3 release.

--- setup.py
+++ setup.py
@@ -28,14 +28,14 @@ else:
 
 setuptools.setup(
     name="django-googledrive-storage",
-    version="1.3.2",
+    version="1.3.3",
     author="Gian Luca Dalla Torre",
     author_email="gianluca.dallatorre@gmail.com",
     description=("Storage implementation for Django that interacts with Google Drive"),
     license="LICENSE.txt",
     keywords="django google drive storage googledrive",
     url="https://github.com/torre76/django-googledrive-storage",
-    download_url="https://github.com/torre76/django-googledrive-storage/tarball/1.3.2",
+    download_url="https://github.com/torre76/django-googledrive-storage/tarball/1.3.3",
     packages=setuptools.find_packages(exclude=["django_googledrive_storage", "gdstorage.tests", "docs"]),
     long_description=long_description,
     package_data={
@@ -49,6 +49,7 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ],
 )

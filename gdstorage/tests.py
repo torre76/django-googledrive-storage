bae564d870e62e75ed4d147fabbdae1252a94ac1
Oleg Smedyuk <conformist.mw@gmail.com>
2021-03-20 22:29:08 +0200

    fixed lint errors, redundant else statements

--- gdstorage/tests.py
+++ gdstorage/tests.py
@@ -1,109 +1,122 @@
 import os
 import os.path
 import time
-from pprint import pprint
 
+import pytest
 from django.test import TestCase
-from gdstorage.storage import GoogleDriveStorage, GoogleDrivePermissionType, GoogleDrivePermissionRole, \
-    GoogleDriveFilePermission
 
+from gdstorage.storage import (GoogleDriveFilePermission,
+                               GoogleDrivePermissionRole,
+                               GoogleDrivePermissionType, GoogleDriveStorage)
 
 SLEEP_INTERVAL = 10
 
 
+@pytest.fixture
+def gds():
+    return GoogleDriveStorage()
+
+
+@pytest.fixture
+def write_perm_gds():
+    return GoogleDriveStorage(
+        permissions=(GoogleDriveFilePermission(
+            GoogleDrivePermissionRole.WRITER,
+            GoogleDrivePermissionType.ANYONE,
+        ),)
+    )
+
+
+@pytest.fixture
+def read_write_perm_gds():
+    return GoogleDriveStorage(
+        permissions=(
+            GoogleDriveFilePermission(
+                GoogleDrivePermissionRole.WRITER,
+                GoogleDrivePermissionType.USER,
+                'foo@mailinator.com',
+            ),
+            GoogleDriveFilePermission(
+                GoogleDrivePermissionRole.READER,
+                GoogleDrivePermissionType.ANYONE,
+            ),
+        )
+    )
+
+
 class GoogleDriveStorageTest(TestCase):
-    def test_check_root_file_exists(self):
-        gds = GoogleDriveStorage()
-        file_data = gds._check_file_exists("How to get started with Drive")
-        pprint(file_data)
-        self.assertIsNotNone(file_data, "Unable to find file 'How to get started with Drive'")
+    def test_check_root_file_exists(self, gds):
+        file_data = gds._check_file_exists('How to get started with Drive')
+        assert file_data, "Unable to find file 'How to get started with Drive'"
         time.sleep(SLEEP_INTERVAL)
 
-    def test_check_or_create_folder(self):
-        gds = GoogleDriveStorage()
-        folder_data = gds._get_or_create_folder("test4/folder")
-        pprint(folder_data)
-        self.assertIsNotNone(folder_data, "Unable to find or create folder 'test4/folder")
+    def test_check_or_create_folder(self, gds):
+        folder_data = gds._get_or_create_folder('test4/folder')
+        assert folder_data, "Unable to find or create folder 'test4/folder'"
         time.sleep(SLEEP_INTERVAL)
 
-    def _test_upload_file(self):
-        gds = GoogleDriveStorage()
-        file_name = "{0}{1}{2}".format(os.path.dirname(os.path.abspath(__file__)), os.path.sep,
-                                       "../test/gdrive_logo.png")
-        result = gds.save("/test4/gdrive_logo.png", open(file_name, 'rb'))
-        pprint(result)
-        self.assertIsNotNone(result, 'Unable to upload file to Google Drive')
+    def _test_upload_file(self, gds):
+        file_name = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '../test/gdrive_logo.png',
+        )
+        with open(file_name, 'rb') as file:
+            result = gds.save('/test4/gdrive_logo.png', file)
+        assert result, 'Unable to upload file to Google Drive'
 
-    def _test_list_folder(self):
+    def _test_list_folder(self, gds):
         self._test_upload_file()
-        gds = GoogleDriveStorage()
-        (directories, files) = gds.listdir("/test4")
-        pprint(directories)
-        pprint(files)
-        self.assertTrue(len(files) > 0, "Unable to read directory data")
+        directories, files = gds.listdir('/test4')
+        assert len(files) > 0, 'Unable to read directory data'
 
-    def _test_open_file(self):
+    def _test_open_file(self, gds):
         self._test_list_folder()
-        gds = GoogleDriveStorage()
-        f = gds.open('/test4/gdrive_logo.png', "rb")
-        pprint(f)
-        pprint(len(f))
-        self.assertIsNotNone(f, "Unable to load data from Google Drive")
+        file = gds.open('/test4/gdrive_logo.png', 'rb')
+        assert file, 'Unable to load data from Google Drive'
 
-    def test_permission_full_write(self):
-        full_write_permission = GoogleDriveFilePermission(GoogleDrivePermissionRole.WRITER,
-                                                          GoogleDrivePermissionType.ANYONE)
-        gds = GoogleDriveStorage(permissions=(full_write_permission,))
-        file_name = "{0}{1}{2}".format(os.path.dirname(os.path.abspath(__file__)), os.path.sep,
-                                       "../test/gdrive_logo.png")
-        result = gds.save("/test4/gdrive_logo.png", open(file_name, 'rb'))
-        pprint(result)
-        self.assertIsNotNone(result, 'Unable to upload file to Google Drive')
-        f = gds.open(result, "rb")
-        pprint(f)
-        pprint(len(f))
-        self.assertIsNotNone(f, "Unable to load data from Google Drive")
+    def test_permission_full_write(self, write_perm_gds):
+        file_name = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '../test/gdrive_logo.png',
+        )
+        with open(file_name) as file:
+            result = write_perm_gds.save('/test4/gdrive_logo.png', file)
+        assert result, 'Unable to upload file to Google Drive'
+        file = write_perm_gds.open(result, 'rb')
+        assert file, 'Unable to load data from Google Drive'
         time.sleep(SLEEP_INTERVAL)
 
-    def test_multiple_permission(self):
-        full_write_to_foo = GoogleDriveFilePermission(GoogleDrivePermissionRole.WRITER,
-                                                      GoogleDrivePermissionType.USER,
-                                                      "foo@mailinator.com")
-        read_only_to_anyone = GoogleDriveFilePermission(GoogleDrivePermissionRole.READER,
-                                                        GoogleDrivePermissionType.ANYONE)
-        gds = GoogleDriveStorage(permissions=(full_write_to_foo, read_only_to_anyone,))
-        file_name = "{0}{1}{2}".format(os.path.dirname(os.path.abspath(__file__)), os.path.sep,
-                                       "../test/gdrive_logo.png")
-        result = gds.save("/test4/gdrive_logo.png", open(file_name, 'rb'))
-        pprint(result)
-        self.assertIsNotNone(result, 'Unable to upload file to Google Drive')
-        f = gds.open(result, "rb")
-        pprint(f)
-        pprint(len(f))
-        self.assertIsNotNone(f, "Unable to load data from Google Drive")
+    def test_multiple_permission(self, read_write_perm_gds):
+
+        file_name = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '../test/gdrive_logo.png',
+        )
+        result = read_write_perm_gds.save(
+            '/test4/gdrive_logo.png', open(file_name, 'rb')
+        )
+        assert result, 'Unable to upload file to Google Drive'
+        file = read_write_perm_gds.open(result, 'rb')
+        assert file, 'Unable to load data from Google Drive'
         time.sleep(SLEEP_INTERVAL)
 
-    def test_upload_big_file(self):
-        gds = GoogleDriveStorage()
-        file_name = "{0}{1}{2}".format(os.path.dirname(os.path.abspath(__file__)), os.path.sep,
-                                       "../test/huge_file")
-        with open(file_name, "wb") as out:
+    def test_upload_big_file(self, gds):
+        file_name = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '../test/huge_file',
+        )
+        with open(file_name, 'wb') as out:
             out.truncate(1024 * 1024 * 20)
 
-        result = gds.save("/test5/huge_file", open(file_name, 'rb'))
-        pprint(result)
-        self.assertIsNotNone(result, 'Unable to upload file to Google Drive')
+        with open(file_name, 'rb') as file:
+            result = gds.save('/test5/huge_file', file)
+        assert result, 'Unable to upload file to Google Drive'
 
         os.remove(file_name)
-
         time.sleep(SLEEP_INTERVAL)
 
-    def test_open_big_file(self):
+    def test_open_big_file(self, gds):
         self._test_list_folder()
-        gds = GoogleDriveStorage()
-        f = gds.open('/test5/huge_file', "rb")
-        pprint(f)
-        pprint(len(f))
-        self.assertIsNotNone(f, "Unable to load data from Google Drive")
+        file = gds.open('/test5/huge_file', 'rb')
+        assert file, 'Unable to load data from Google Drive'
         time.sleep(SLEEP_INTERVAL)
-
